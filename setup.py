c73a2ce6a7185b37c20960373e3efa79de9558e5
SaashaJoshi <saashajoshi08@gmail.com>
2024-02-08 12:20:34 -0800

    initial directory structure and gitignore commits

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""MLflow-Qiskit setup file."""
+from setuptools import setup
+
+setup(
+    name="mlflow_qiskit",
+    version="0.1",
+    description="A wrapper for MLflow and Qiskit",
+    author="Saasha Joshi",
+    author_email="saashajoshi08@gmail.com",
+    url="https://github.com/SaashaJoshi/mlflow-qiskit-wrapper",
+    packages=["mlflow_qiskit"],
+)
